fc6f67ab530bc9f6a7a3a28fe7ecf728c36c6ba6
RiyanPC <locochon56303@gmail.com>
2025-10-15 19:40:10 -0500

    arreglando tu sistema -_-

--- app/Views/layouts.php
+++ app/Views/layouts.php
@@ -8,15 +8,23 @@
   <title>Hola Mundo - MVC + PHP + CRUD</title>
   <link rel="icon" href="<?= BASE_URL ? rtrim(BASE_URL, '/') : '' ?>/imagenes/mensaje.img.png">
   <link rel="stylesheet" href="<?= BASE_URL ? rtrim(BASE_URL, '/') : '' ?>/assets/css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
   <header class="container">
     <h1>
-      <a href="<?= BASE_URL ? rtrim(BASE_URL, '/') : '' ?>/"> 👋hola mundo - MVC + PHP + CRUD</a>
+      <a href="<?= BASE_URL ? rtrim(BASE_URL, '/') : '' ?>/">
+        <svg class="icon" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>
+        Hola Mundo - MVC + PHP + CRUD
+      </a>
     </h1>
-    <nav>
-      <a href="<?= BASE_URL ? rtrim(BASE_URL, '/') : '' ?>/mensajes"> Mensajes</a>
-      <a href="<?= BASE_URL ? rtrim(BASE_URL, '/') : '' ?>/crear"> Crear Mensaje</a>
+  <nav>
+      <a href="<?= BASE_URL ? rtrim(BASE_URL, '/') : '' ?>/mensaje">
+        <i class="fas fa-list"></i> Mensajes
+      </a>
+      <a href="<?= BASE_URL ? rtrim(BASE_URL, '/') : '' ?>/mensaje/create">
+        <i class="fas fa-plus"></i> Crear Mensaje
+      </a>
     </nav>
   </header>
   
